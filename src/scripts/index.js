bc4bb1da21de6c3300807ddda1c353812ffd4136
Yonanda Putra <93269134+yonand21@users.noreply.github.com>
2023-04-19 13:52:03 +0800

    Update index.js

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,6 @@
 import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.css';
 import '../scripts/navbar.js';
+import '../scripts/cardlist.js'
 
 console.log('Hello Coders! :)');
